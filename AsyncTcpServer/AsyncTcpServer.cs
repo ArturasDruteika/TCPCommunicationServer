0d6d6b4a0e3fb5a8b5b2e17ec2ea7af60708eaac
ArtūrasDrūteika <arturas.druteika@gmail.com>
2023-12-25 15:00:13 +0200

    Added list to store all the clients
    Added Print operation for clients info

--- AsyncTcpServer/AsyncTcpServer.cs
+++ AsyncTcpServer/AsyncTcpServer.cs
@@ -1,72 +1,122 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-public class AsyncTcpServer
+
+namespace Server
 {
-    private TcpListener _listener;
-    private bool _isRunning;
-
-    public AsyncTcpServer(string ipAddress, int port)
+    public class AsyncTcpServer
     {
-        IPAddress ip = IPAddress.Parse(ipAddress);
-        _listener = new TcpListener(ip, port);
-    }
+        private TcpListener _listener;
+        private bool _isRunning;
+        private List<TcpClient> _totalCLientsConnected = new List<TcpClient>();
+        private readonly object _clientsLock = new object();
 
-    public void Start()
-    {
-        _listener.Start();
-        _isRunning = true;
-        Console.WriteLine("Server started. Listening for connections...");
-
-        AcceptClientsAsync();
-    }
-
-    private async void AcceptClientsAsync()
-    {
-        while (_isRunning)
+        public AsyncTcpServer(string ipAddress, int port)
         {
-            TcpClient client = await _listener.AcceptTcpClientAsync();
-            Console.WriteLine("Client connected.");
-            // Handle the client in a new task
-            _ = Task.Run(() => HandleClientAsync(client));
+            IPAddress ip = IPAddress.Parse(ipAddress);
+            _listener = new TcpListener(ip, port);
         }
-    }
 
-    private async Task HandleClientAsync(TcpClient client)
-    {
-        try
+        public void Start()
         {
-            using NetworkStream stream = client.GetStream();
-            byte[] buffer = new byte[1024];
-            int bytesRead;
+            _listener.Start();
+            _isRunning = true;
+            Console.WriteLine("Server started. Listening for connections...");
 
-            while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+            AcceptClientsAsync();
+        }
+
+        private async Task HandleClientAsync(TcpClient client)
+        {
+            try
             {
-                string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                Console.WriteLine("Received: " + receivedData);
+                using NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                int bytesRead;
 
-                // Echo the data back to the client
-                byte[] sendData = Encoding.UTF8.GetBytes(receivedData);
-                await stream.WriteAsync(sendData, 0, sendData.Length);
+                while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                {
+                    string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("Received: " + receivedData);
+
+                    byte[] sendData = Encoding.UTF8.GetBytes(receivedData);
+                    await stream.WriteAsync(sendData, 0, sendData.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+            finally
+            {
+                client.Close();
+                lock (_clientsLock)
+                {
+                    _totalCLientsConnected.Remove(client);
+                }
             }
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Exception: " + ex.Message);
-        }
-        finally
-        {
-            client.Close();
-        }
-    }
 
-    public void Stop()
-    {
-        _isRunning = false;
-        _listener.Stop();
-        Console.WriteLine("Server stopped.");
+        public void Stop()
+        {
+            PrintClientsInfo();
+            _isRunning = false;
+            _listener.Stop();
+            Console.WriteLine("Server stopped.");
+        }
+
+        private async void AcceptClientsAsync()
+        {
+            while (_isRunning)
+            {
+                TcpClient client = await _listener.AcceptTcpClientAsync();
+                Console.WriteLine("Client connected.");
+                lock (_clientsLock)
+                {
+                    _totalCLientsConnected.Add(client);
+                    PrintClientInfo(client);
+                }
+                _ = Task.Run(() => HandleClientAsync(client));
+            }
+        }
+
+        private void PrintClientInfo(TcpClient client)
+        {
+            lock (_clientsLock)
+            {
+                if (client.Connected)
+                {
+                    Console.WriteLine("Client:");
+                    Console.WriteLine("  Local Endpoint: " + client.Client.LocalEndPoint);
+                    Console.WriteLine("  Remote Endpoint: " + client.Client.RemoteEndPoint);
+                }
+                else
+                {
+                    Console.WriteLine("Client not connected.");
+                }
+            }
+        }
+
+        private void PrintClientsInfo()
+        {
+            lock (_clientsLock)
+            {
+                if (_totalCLientsConnected.Count == 0)
+                {
+                    Console.WriteLine("All clients have disconnected.");
+                }
+                else
+                {
+                    foreach (TcpClient client in _totalCLientsConnected)
+                    {
+                        PrintClientInfo(client);
+                    }
+                }
+            }
+        }
     }
 }
