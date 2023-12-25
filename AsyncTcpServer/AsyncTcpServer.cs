87d6069f55b2b4613879a01386d225b32ce3f538
ArtūrasDrūteika <arturas.druteika@gmail.com>
2023-12-25 21:29:33 +0200

    Implemented image receiving from client + saving

--- AsyncTcpServer/AsyncTcpServer.cs
+++ AsyncTcpServer/AsyncTcpServer.cs
@@ -2,21 +2,35 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 
-namespace Server
+namespace CustomServer
 {
     public class AsyncTcpServer
     {
+        private readonly string _ip = "";
+        private readonly int _port = 0;
         private TcpListener _listener;
-        private bool _isRunning;
+        private bool _isRunning = false;
         private List<TcpClient> _totalCLientsConnected = new List<TcpClient>();
         private readonly object _clientsLock = new object();
+        private int _receivedFileCount = 0;
+        private readonly string _imageSavePath = "";
+        private CancellationTokenSource _cts = new CancellationTokenSource();
 
         public AsyncTcpServer(string ipAddress, int port)
         {
+            _ip = ipAddress;
+            _port = port;
+
+            string executablePath = Assembly.GetExecutingAssembly().Location;
+            string executableDirectory = Path.GetDirectoryName(executablePath);
+            _imageSavePath = Path.Combine(executableDirectory, "data", "images");
+            Directory.CreateDirectory(_imageSavePath);
+
             IPAddress ip = IPAddress.Parse(ipAddress);
             _listener = new TcpListener(ip, port);
         }
@@ -26,15 +40,22 @@ namespace Server
             _listener.Start();
             _isRunning = true;
             Console.WriteLine("Server started. Listening for connections...");
-
             AcceptClientsAsync();
         }
 
-        private async Task HandleClientAsync(TcpClient client)
+        public void Stop()
+        {
+            PrintClientsInfo();
+            _isRunning = false;
+            _cts.Cancel();
+            _listener.Stop();
+            Console.WriteLine("Server stopped.");
+        }
+
+        private async Task HandleStrMsgAsync(NetworkStream stream)
         {
             try
             {
-                using NetworkStream stream = client.GetStream();
                 byte[] buffer = new byte[1024];
                 int bytesRead;
 
@@ -51,36 +72,69 @@ namespace Server
             {
                 Console.WriteLine("Exception: " + ex.Message);
             }
-            finally
-            {
-                client.Close();
-                lock (_clientsLock)
-                {
-                    _totalCLientsConnected.Remove(client);
-                }
-            }
         }
 
-        public void Stop()
+        public async Task HandleImgMsgAsync(NetworkStream stream)
         {
-            PrintClientsInfo();
-            _isRunning = false;
-            _listener.Stop();
-            Console.WriteLine("Server stopped.");
+            // Specify the path and name for the saved image
+            string imagePath = _imageSavePath + "/received_image.jpg";
+            using var fileStream = new FileStream(imagePath, FileMode.Create);
+
+            // Read the image data from the stream and write it to the file
+            await stream.CopyToAsync(fileStream);
+
+            Console.WriteLine($"Image saved to {imagePath}");
+            // Further processing of the image...
+        }
+
+        private async Task HandleClientAsync(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[3]; // Adjusted to 3 bytes for the "IMG" header
+
+            // Read the header
+            await stream.ReadAsync(buffer, 0, buffer.Length);
+            string header = Encoding.ASCII.GetString(buffer);
+
+            if (header == CommandTypes.MSG)
+            {
+                await HandleStrMsgAsync(stream);
+            }
+            else if (header == CommandTypes.IMG)
+            {
+                await HandleImgMsgAsync(stream);
+            }
+
+            client.Close();
         }
 
         private async void AcceptClientsAsync()
         {
-            while (_isRunning)
+            try
             {
-                TcpClient client = await _listener.AcceptTcpClientAsync();
-                Console.WriteLine("Client connected.");
-                lock (_clientsLock)
+                while (!_cts.Token.IsCancellationRequested)
                 {
-                    _totalCLientsConnected.Add(client);
-                    PrintClientInfo(client);
+                    if (_listener.Pending())
+                    {
+                        TcpClient client = await _listener.AcceptTcpClientAsync();
+                        Console.WriteLine("Client connected.");
+                        lock (_clientsLock)
+                        {
+                            _totalCLientsConnected.Add(client);
+                            PrintClientInfo(client);
+                        }
+                        _ = Task.Run(() => HandleClientAsync(client));
+                    }
+                    else
+                    {
+                        await Task.Delay(100); // Wait a bit before checking again
+                    }
                 }
-                _ = Task.Run(() => HandleClientAsync(client));
+            }
+            catch (ObjectDisposedException)
+            {
+                // Listener has been stopped
+                Console.WriteLine("Listener has been stopped.");
             }
         }
 
