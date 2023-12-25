b58e0399af923d66c1b34ef9a12df1ca20ba05ed
ArtūrasDrūteika <arturas.druteika@gmail.com>
2023-12-25 14:04:13 +0200

    Added asynchronous client handling server to program

---
+++ AsyncTcpServer/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        AsyncTcpServer server = new AsyncTcpServer("127.0.0.1", 1024);
+        server.Start();
+
+        Console.WriteLine("Press Enter to stop the server...");
+        Console.ReadLine();
+
+        server.Stop();
+    }
+}
