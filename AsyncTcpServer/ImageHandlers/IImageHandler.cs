00ff5eeb424a8b6ec441831bbfbd150c7958c04b
ArtūrasDrūteika <arturas.druteika@gmail.com>
2023-12-26 17:26:56 +0200

    Added image handling classes

---
+++ AsyncTcpServer/ImageHandlers/IImageHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Sockets;
+
+
+namespace AsyncTcpServer.ImageHandlers
+{
+    public interface IImageHandler
+    {
+        Task HandleImageAsync(NetworkStream stream, string imgPath);
+    }
+}
